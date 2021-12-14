84b23daef9b5006c68bee13f9c0b61bdc4716832
shaonlt <706736163@qq.com>
2021-12-14 15:40:48 +0800

    添加恶魔圈

--- dancing_ball/main.js
+++ dancing_ball/main.js
@@ -1,3 +1,6 @@
+const para = document.querySelector('p');
+let count = 0;
+
 // 设置画布
 
 const canvas = document.querySelector('canvas');
@@ -20,14 +23,23 @@ function randomColor() {
     random(0, 255) + ')';
 }
 
-function Ball (x, y, velX, velY, color, size) {
+function Shape (x, y, velX, velY, exists) {
   this.x = x;
   this.y = y;
   this.velX = velX;
   this.velY = velY;
+  this.exists = exists;
+}
+
+function Ball(x, y, velX, velY, exists, color, size) {
+  Shape.call(this, x, y, velX, velY, exists);
+
   this.color = color;
   this.size = size;
 }
+
+Ball.prototype = Object.create(Shape.prototype);
+Ball.prototype.constructor = Ball;
 
 Ball.prototype.draw = function() {
   ctx.beginPath();
@@ -71,6 +83,85 @@ Ball.prototype.collisionDetect = function() {
   }
 }
 
+function EvilBall(x, y, velX, velY, exists, color, size) {
+  Shape.call(this, x, y, 20, 20, true);
+
+  this.color = 'white'
+  this.size = 20;
+}
+
+EvilBall.prototype = Object.create(Shape.prototype);
+EvilBall.prototype.constructor = EvilBall;
+
+EvilBall.prototype.draw = function() {
+  ctx.beginPath();
+  ctx.lineWidth = 5;
+  ctx.strokeStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.stroke();
+};
+
+EvilBall.prototype.update = function() {
+  if ((this.x + this.size) >= width) {
+    this.x -= this.velX;
+  }
+
+  if ((this.x - this.size) <= 0) {
+    this.x += this.velX;
+  }
+
+  if ((this.y + this.size) >= height) {
+    this.y -= this.velY;
+  }
+
+  if ((this.y - this.size) <= 0) {
+    this.y += this.velY;
+  }
+};
+
+EvilBall.prototype.setControls = function() {
+  window.onkeydown = e => {
+    switch(e.key) {
+      case 'a':
+      case 'A':
+      case 'ArrowLeft':
+        this.x -= this.velX;
+        break;
+      case 'd':
+      case 'D':
+      case 'ArrowRight':
+        this.x += this.velX;
+        break;
+      case 'w':
+      case 'W':
+      case 'ArrowUp':
+        this.y -= this.velY;
+        break;
+      case 's':
+      case 'S':
+      case 'ArrowDown':
+        this.y += this.velY;
+        break;
+    }
+  };
+}
+
+EvilBall.prototype.collisionDetect = function() {
+  for (let j = 0; j < balls.length; j++) {
+    if (balls[j].exists) {
+      const dx = this.x - balls[j].x;
+      const dy = this.y - balls[j].y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + balls[j].size) {
+        balls[j].exists = false;
+        count--;
+        para.textContent = '剩余彩球数：' + count;
+      }
+    }
+  }
+}
+
 let balls = [];
 
 while (balls.length < 25) {
@@ -80,21 +171,33 @@ while (balls.length < 25) {
     random(0 + size, height - size),
     random(-7, 7),
     random(-7, 7),
+    true,
     randomColor(),
     size
   );
   balls.push(ball);
+  count++;
+  para.textContent = '剩余彩球数：' + count;
 }
+
+let evil = new EvilBall(random(0, width), random(0, height), true);
+evil.setControls();
 
 function loop() {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
   ctx.fillRect(0, 0, width, height);
 
   for (let i = 0; i < balls.length; i++) {
-    balls[i].draw();
-    balls[i].update();
-    balls[i].collisionDetect();
+    if (balls[i].exists) {
+      balls[i].draw();
+      balls[i].update();
+      balls[i].collisionDetect();
+    }
   }
+  
+  evil.draw();
+  evil.update();
+  evil.collisionDetect();
 
   requestAnimationFrame(loop);
 }
